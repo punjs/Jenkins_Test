c71010b4798337d51249450d7b4cc1210ecdaa83
punjs <42285800+punjs@users.noreply.github.com>
2021-07-25 17:57:18 +0530

    Update GithubTest.java
    Added line System.out.in

--- src/test/java/com/sandeep/GithubTest/GithubTest.java
+++ src/test/java/com/sandeep/GithubTest/GithubTest.java
@@ -38,6 +38,8 @@ public class GithubTest
         System.out.println("I am executing webhook of Github weekhook");
         Assert.assertEquals(true, true);
         System.out.println("Hi.....");
+        System.out.println("Hi.....");
+        
 
     }
 }
